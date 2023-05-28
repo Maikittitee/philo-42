eaad3eb2f131ccb88e288333e34f5d0faf1f048b
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-05-29 04:49:33 +0700

    feat: input

---
+++ src/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 04:32:52 by ktunchar          #+#    #+#             */
+/*   Updated: 2023/05/29 04:49:12 by ktunchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include "lib/libft/libft.h"
+# include "lib/libftprintf/ft_printf.h"
+
+
+// error
+int	check_arg(int ac, char **av);
+
+
+#endif
