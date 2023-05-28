3c117e748f32fa0a7d94f92cec01dd89e05c2d82
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-05-29 04:53:39 +0700

    feat : check argument that all is digit

--- src/check_arg.c
+++ src/check_arg.c
@@ -6,13 +6,37 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 04:47:44 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/05/29 04:48:44 by ktunchar         ###   ########.fr       */
+/*   Updated: 2023/05/29 04:53:13 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+
+static int	is_str_digit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	check_arg(int ac, char **av)
 {
+	int	i;
+
+	i = 0;
+	while (av[i])
+	{
+		if (!is_str_digit(av[i]))
+			return (0);
+		i++;
+	}
 	return (1);
 }
