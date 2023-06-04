d14104ff94a5e42861fd87e52802369b75e3719e
ktunchar <maikittitee1615@gmail.com>
2023-06-04 23:04:46 +0700

    fix : semi

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 22:50:26 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/06/04 23:04:09 by ktunchar         ###   ########.fr       */
+/*   Updated: 2023/06/04 23:04:37 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	take_fork(t_data *data, t_philo *philo)
 		return (0);
 	//print take right hand fork
 	if (philo->status == DIED || pthread_mutex_lock(&(data->forks[philo->left])) != 0)
-		return (0)	
+		return (0);	
 	//print take right hand fork
 	return (1);
 		
