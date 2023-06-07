a106168624630a05e339d00530cf9a088a37554c
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-06-08 02:03:09 +0700

    feat: clean routine

--- src/philo.h
+++ src/philo.h
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 04:32:52 by ktunchar          #+#    #+#             */
-/*   Updated: 2023/06/06 22:55:33 by ktunchar         ###   ########.fr       */
+/*   Updated: 2023/06/07 23:23:33 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ typedef struct s_philo{
 	int	right;
 	int	n_fork;
 	int	n_eat;
+	int	last_eat_ms;
 	
 }t_philo;
 
@@ -56,12 +57,12 @@ typedef struct s_data{
 #define EATING 1
 #define THINKING 2
 #define SLEEPING 3
-#define	T_DIED "\e[0;31m%ld philo%d Died\e[0m\n"
-#define	T_LTAKING "%ld philo%d is taking left fork\n"
-#define	T_RTAKING "%ld philo%d is taking right fork\n"
-#define	T_EATING "%ld philo%d is Eating\n"
-#define	T_THINKING "%ld philo%d is Thinking\n"
-#define	T_SLEEPING "%ld philo%d is Sleeping\n"
+#define	T_DIED "\e[0;31m%ldms philo%d Died\e[0m\n"
+#define	T_LTAKING "%ldms philo%d is taking left fork\n"
+#define	T_RTAKING "%ldms philo%d is taking right fork\n"
+#define	T_EATING "%ldms philo%d is Eating\n"
+#define	T_THINKING "%ldms philo%d is Thinking\n"
+#define	T_SLEEPING "%ldms philo%d is Sleeping\n"
 // error
 int		check_arg(int ac, char **av);
 
--- src/routine.c
+++ src/routine.c
@@ -19,67 +19,42 @@ int	take_fork(t_data *data, t_philo *philo)
 		return (0);
 	}
 	//print take left hand fork
-	printf(T_LTAKING, ms_from_start(data), philo->id);
+	printf(T_RTAKING, ms_from_start(data), philo->id);
 	philo->n_fork += 1; // need to init n_fork = 0;
 	if (data->die_flag || pthread_mutex_lock(&data->forks[philo->left]) != 0)
 		return (0);	
 	//print take right hand fork
-	printf(T_RTAKING, ms_from_start(data), philo->id);
+	printf(T_LTAKING, ms_from_start(data), philo->id);
 	philo->n_fork += 1;
 	return (1);
 		
 }
-void	ft_eat(t_data *data, t_philo *philo)
+
+int	ft_eat(t_data *data, t_philo *philo)
 {
 	long init_time;
 
 	philo->status = EATING;
 	init_time = ms_from_epoch();
+	philo->n_eat += 1;
 	printf(T_EATING, ms_from_start(data), philo->id);
-	while (philo->n_fork == 2 && !data->die_flag && ms_from_epoch() - init_time < data->arg->t_eat)
+	while (!data->die_flag && ms_from_epoch() - init_time < data->arg->t_eat)
 		usleep(50);
-	
+	return (data->die_flag);
 }
 
-void ft_sleep(t_data *data, t_philo *philo)
+int ft_sleep(t_data *data, t_philo *philo)
 {
 	long init_time;
 
 	philo->status = SLEEPING;
-	if (data->die_flag)
-		return ;
 	init_time = ms_from_epoch();
 	printf(T_SLEEPING, ms_from_start(data), philo->id);
 	while (!data->die_flag && ms_from_epoch() - init_time < data->arg->t_sleep)
 		usleep(50);
+	return (data->die_flag);
 }
 
-void ft_think(t_data *data, t_philo *philo)
-{
-	long init_time;
-
-	philo->status = SLEEPING;
-	if (data->die_flag)
-		return ;
-	init_time = ms_from_epoch();
-	printf(T_THINKING, ms_from_start(data), philo->id);
-	while (!data->die_flag && ms_from_epoch() - init_time < data->arg->t_sleep)
-		usleep(50);
-}
-
-
-
-void	ft_die(t_data *data, t_philo *philo)
-{
-	// time to die , max eat
-	if (data->die_flag)
-		return ;
-	if (ms_from_start(data) > data->arg->t_die || philo->n_eat == data->arg->max_eat)
-	{
-		printf(T_DIED, ms_from_start(data), philo->id);
-		data->die_flag = 1;
-	}
-}
 
 int	put_fork_down(t_data *data, t_philo *philo)
 {
@@ -102,17 +77,20 @@ void	*routine(void *arg)
 	data->philos[i].id = i + 1;
 	data->philos[i].left = i;
 	data->philos[i].right = (i + 1) % data->arg->n_philo;
-	printf("hello from philo index %d,i have %d %d\n", i, data->philos[i].left, data->philos[i].right);
+	data->philos[i].n_eat = 0;
+	// printf("hello from philo index %d,i have %d %d\n", i, data->philos[i].left, data->philos[i].right);
 	while (!data->die_flag)
 	{
 		take_fork(data, &(data->philos[i]));
 		// check if 2 fork in the hands -> eat
-		// if (data->philos[i].n_fork == 2)
-		ft_eat(data, &(data->philos[i]));
+		data->philos[i].last_eat_ms = ms_from_start(data);
+		if (ft_eat(data, &(data->philos[i])))
+			return (0);
 		put_fork_down(data, &(data->philos[i]));
-		ft_sleep(data, &(data->philos[i]));
-		ft_think(data, &(data->philos[i]));
-		ft_die(data, &(data->philos[i]));
+
+		if (ft_sleep(data, &(data->philos[i])))
+			return (0);
+		printf(T_THINKING, ms_from_start(data), data->philos[i].id);
 	}
 	return (0);
 }
